7bcdcc991a64c1a20da0829294cbc6c513bd861e
CyberFather <maks12ate@gmail.com>
2023-10-16 19:17:37 +0300

    nothing to see here

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
-export default defineConfig(async () => ({
-  plugins: [svelte()],
+defineConfig(async () => ({
+   plugins: [svelte()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
