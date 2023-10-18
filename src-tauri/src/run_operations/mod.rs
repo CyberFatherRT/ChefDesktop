35ea80fd433c3fb8abd928d2712cfb0a0c0ef11f
CyberFather <maks12ate@gmail.com>
2023-10-18 21:16:12 +0300

    update add_line_numbers operation

--- src-tauri/src/run_operations/mod.rs
+++ src-tauri/src/run_operations/mod.rs
@@ -76,3 +76,5 @@ mod a1z26_cipher_decode;
 pub use a1z26_cipher_decode::a1z26_cipher_decode;
 mod a1z26_cipher_encode;
 pub use a1z26_cipher_encode::a1z26_cipher_encode;
+mod add_line_numbers;
+pub use add_line_numbers::add_line_numbers;
