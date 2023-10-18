82232cddfdf985e4bfb2dccbcd43ac038d54cfd2
CyberFather <maks12ate@gmail.com>
2023-10-18 21:16:12 +0300

    update analyse_hash operation

--- src-tauri/src/run_operations/analyse_hash.rs
+++ src-tauri/src/run_operations/analyse_hash.rs
@@ -1,6 +1,7 @@
-pub use operations::{run_operations, AnalyseHash};
+pub use operations::{run_operations, };
 
 #[tauri::command]
 pub fn analyse_hash(request: &str) -> Result<String, String> {
-    run_operations(AnalyseHash, request)
+    run_operations(, request)
 }
+
