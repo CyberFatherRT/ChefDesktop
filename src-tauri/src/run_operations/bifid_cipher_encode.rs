d02c7b0fbc9180d90c51ae05cfe21bfbc2ede2cf
CyberFather <maks12ate@gmail.com>
2023-10-18 21:16:12 +0300

    update bifid_cipher_encode operation

--- src-tauri/src/run_operations/bifid_cipher_encode.rs
+++ src-tauri/src/run_operations/bifid_cipher_encode.rs
@@ -1,6 +1,7 @@
-pub use operations::{run_operations, BifidCipherEncode};
+pub use operations::{run_operations, };
 
 #[tauri::command]
 pub fn bifid_cipher_encode(request: &str) -> Result<String, String> {
-    run_operations(BifidCipherEncode, request)
+    run_operations(, request)
 }
+
