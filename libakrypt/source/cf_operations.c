52d4ac551c87650ca74e39e5523c6c2513b3d3e4
CyberFather <maks12ate@gmail.com>
2024-02-05 22:20:16 +0300

    da

--- libakrypt/source/cf_operations.c
+++ libakrypt/source/cf_operations.c
@@ -1,7 +1,34 @@
-#include <stdio.h>
+#include <libakrypt-base.h>
+#include <libakrypt.h>
 
-char* foo() {
-    printf("Hello from libakrypt!");
-    return "Hello from libakrypt!";
+/*
+*
+* kuznechik / magma - cbc, ctr, ofb, cfb, acpkm, xts
+*
+*/
+
+int foo() {
+    struct bckey ctx;
+
+    if (!ak_libakrypt_create(ak_function_log_syslog)) {
+        return ak_libakrypt_destroy();
+    }
+
+    ak_libakrypt_set_openssl_compability(ak_false);
+
+    ak_bckey_create_kuznechik(&ctx);
+
+    ak_bckey_set_key(&ctx, "super_secret_key", 16);
+
+    char data[] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+
+    ak_bckey_encrypt_ecb(&ctx, data, data, sizeof(data));
+
+    printf("Encrypted: %s\n", ak_ptr_to_hexstr(data, sizeof(data), ak_false));
+
+    ak_bckey_decrypt_ecb(&ctx, data, data, sizeof(data));
+
+    printf("Decrypted: %s\n", data);
+
+    return 0;
 }
-
--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,5 +1,6 @@
 fn main() {
     println!("cargo:rustc-link-search={}/../libakrypt/build", std::env::var("CARGO_MANIFEST_DIR").unwrap());
     println!("cargo:rustc-link-lib=static=akrypt");
+    println!("cargo:rustc-link-lib=static=akrypt-base");
     tauri_build::build()
 }
