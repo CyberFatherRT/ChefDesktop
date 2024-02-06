aa844beccca8935ad4d786d8a1537e60a95ce602
CyberFather <maks12ate@gmail.com>
2024-02-06 13:43:05 +0300

    da

--- libakrypt/source/cf_operations.c
+++ libakrypt/source/cf_operations.c
@@ -1,14 +1,39 @@
 #include <libakrypt-base.h>
 #include <libakrypt.h>
+#include <stdlib.h>
+#include <string.h>
 
 /*
 *
-* kuznechik / magma - cbc, ctr, ofb, cfb, acpkm, xts
+* kuznechik / magma - cbc, ctr, ofb, cfb
 *
 */
 
-int foo() {
+typedef enum {
+    m_cbc,
+    m_ctr,
+    m_ofb,
+    m_cfb,
+} Mode;
+
+typedef struct {
+    char* algorithm;
+    Mode mode;
+    ak_uint8* key;
+    ak_uint8* iv;
+} Config;
+
+
+int akrypt_encrypt(const Config* config, char* input, char* output) {
+
     struct bckey ctx;
+    ak_uint8* key = config->key;
+    ak_uint8* iv = config->iv;
+    char* algorithm = config->algorithm;
+    Mode mode = config->mode;
+
+    int error = ak_error_ok;
+    int exitstatus = EXIT_FAILURE;
 
     if (!ak_libakrypt_create(ak_function_log_syslog)) {
         return ak_libakrypt_destroy();
@@ -16,19 +41,49 @@ int foo() {
 
     ak_libakrypt_set_openssl_compability(ak_false);
 
-    ak_bckey_create_kuznechik(&ctx);
+    if (strcmp(algorithm, "kuznechik")) {
+        ak_bckey_create_kuznechik(&ctx);
+    } else if (strcmp(algorithm, "magma")) {
+        ak_bckey_create_magma(&ctx);
+    } else {
+        char* err_msg = (char *)malloc((20 + strlen(algorithm)) * sizeof(char));
+        sprintf(err_msg, "Unknown algorithm: %s", algorithm);
+        ak_log_set_message(err_msg);
+        free(err_msg);
+        return -1;
+    }
 
-    ak_bckey_set_key(&ctx, "super_secret_key", 16);
+    ak_bckey_set_key(&ctx, key, sizeof(key));
 
-    char data[] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+    switch (mode) {
 
-    ak_bckey_encrypt_ecb(&ctx, data, data, sizeof(data));
+        case m_cbc:
+            error = ak_bckey_encrypt_cbc(&ctx, input, output, sizeof(input), iv, sizeof(iv));
+            if (error != ak_error_ok) goto exlab;
+            break;
+        case m_ctr:
+            error = ak_bckey_ctr(&ctx, input, output, sizeof(input), iv, sizeof(iv));
+            if (error != ak_error_ok) goto exlab;
+            break;
+        case m_ofb:
+            error = ak_bckey_ofb(&ctx, input, output, sizeof(input), iv, sizeof(iv));
+            if (error != ak_error_ok) goto exlab;
+            break;
+        case m_cfb:
+            error = ak_bckey_encrypt_cfb(&ctx, input, output, sizeof(input), iv, sizeof(iv));
+            if (error != ak_error_ok) goto exlab;
+            break;
+    }
 
-    printf("Encrypted: %s\n", ak_ptr_to_hexstr(data, sizeof(data), ak_false));
+exlab: ak_bckey_destroy(&ctx);
 
-    ak_bckey_decrypt_ecb(&ctx, data, data, sizeof(data));
+    if (error == ak_error_ok) exitstatus = EXIT_SUCCESS;
 
-    printf("Decrypted: %s\n", data);
+    ak_libakrypt_destroy();
 
-    return 0;
+    return exitstatus;
+}
+
+void foo() {
+    printf("Hello from akrypt");
 }
--- src/lib/core/baseOperation.ts
+++ src/lib/core/baseOperation.ts
@@ -49,3 +49,4 @@ export enum UserInputOptions {
     checkbox,
     intCounter,
 }
+
