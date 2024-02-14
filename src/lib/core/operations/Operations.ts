fdeb9fba1644e320aa23d01855c9d4ca28a78f5e
CyberFather <maks12ate@gmail.com>
2024-02-14 21:29:04 +0300

    Create file for all TS operations

---
+++ src/lib/core/operations/Operations.ts
@@ -0,0 +1,10 @@
+import { Hmac } from "./Hmac";
+import { KuznechikDecrypt } from "./KuznechickDecrypt";
+import { KuznechikEncrypt } from "./KuznechickEncrypt";
+
+export {
+    Hmac,
+    KuznechikEncrypt,
+    KuznechikDecrypt,
+}
+
---
+++ src/lib/web/components/main/RecipeList.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import { get } from "svelte/store";
+    import RecipeOperation from "./RecipeOperation.svelte";
+    import { KuznechikEncrypt, KuznechikDecrypt } from "../../../core/operations/Operations";
+    import type { Operation } from "../../../core/baseOperation";
+    import { operations } from "../../../core/runOperations";
+
+    let foo = [
+        new KuznechikEncrypt() as Operation,
+        new KuznechikDecrypt() as Operation,
+    ];
+
+    operations.set(foo);
+
+</script>
+
+<ul class="recipe-list">
+    {#each get(operations) as operation}
+        <RecipeOperation operation={operation}/>
+    {/each}
+</ul>
+
+<style>
+
+    .recipe-list {
+        width: 100%;
+        height: 100%;
+    }
+
+</style>
