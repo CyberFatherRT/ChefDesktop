a952ee68f20169a1e280e53c1c51c50125889a4a
CyberFather <maks12ate@gmail.com>
2024-02-17 16:23:54 +0300

    add FromBase64 TS operation

--- src/lib/core/operations/Operations.ts
+++ src/lib/core/operations/Operations.ts
@@ -1,3 +1,4 @@
+import { FromBase64 } from "./FromBase64";
 import { Hmac } from "./Hmac";
 import { KuznechikDecrypt } from "./KuznechickDecrypt";
 import { KuznechikEncrypt } from "./KuznechickEncrypt";
@@ -6,6 +7,7 @@ import { ToBase64 } from "./ToBase64";
 // @ts-ignore
 export let operationList = {
     "To Base64": ToBase64,
+    "From Base64": FromBase64,
     "HMAC": Hmac,
     "Kuznechik Encrypt": KuznechikEncrypt,
     "Kuznechik Decrypt": KuznechikDecrypt,
