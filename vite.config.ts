86ce2135ff6a433c1239545903ba41e6d1fdd491
CyberFather <maks12ate@gmail.com>
2024-02-17 16:52:45 +0300

    change: change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
-export default defineConfig(async () => ({
+export default defineConfig({
     plugins: [svelte()],
 
     // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
@@ -17,13 +17,9 @@ export default defineConfig(async () => ({
     // 3. to make use of `TAURI_DEBUG` and other env variables
     // https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
     envPrefix: ["VITE_", "TAURI_"],
-    test: {
-        environment: 'jsdom',
-        globals: true
-    },
 
     build: {
         target: process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "safari13",
         minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
     }
-}));
+});
