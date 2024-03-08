171ef8d046f192bd82c4ebfcc600356bdb6c3641
CyberFather <maks12ate@gmail.com>
2024-03-08 10:39:16 +0300

    add prettier formatting

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+/** @type {import("prettier").Config} */
+export default {
+    semi: true,
+    singleQuote: false,
+    tabWidth: 4,
+    trailingComma: "es5",
+    plugins: ["prettier-plugin-svelte"]
+}
